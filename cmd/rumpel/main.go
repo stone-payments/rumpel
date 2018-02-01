ea275080a082669554f4a7d4a352aa5c281013bf
Guilherme Paixão <guiferpa@gmail.com>
2018-02-01 11:32:55 -0200

    Create cmd package to entrypoint application

---
+++ cmd/rumpel/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/stone-payments/rumpel/environment"
+	"github.com/stone-payments/rumpel/rule"
+)
+
+func main() {
+	env, err := environment.Read(os.Getenv("RUMPEL_MODE"), os.Args)
+	if err != nil {
+		log.Fatalf("\n%v", err)
+	}
+
+	rls, err := rule.Config(env.RulesConfigPath)
+	if err != nil {
+		log.Fatalf("Read config rule file error: %v", err)
+	}
+
+	t := template.Must(template.New("welcome").Parse(`
+   ____                             _
+  |  _ \ _   _ _ __ ___  _ __   ___| |
+  | |_) | | | | '_ ' _ \| '_ \ / _ \ |
+  |  _ /| |_| | | | | | | |_) |  __/ |
+  |_| \_\\__,_|_| |_| |_| .__/ \___|_|
+                        |_|
+
+  Environment mode:  {{.Name}}
+  Rule config path:  {{.RulesConfigPath}}
+  Running on port:   {{.ApplicationPort}}
+
+`))
+	if err := t.Execute(os.Stdout, env); err != nil {
+		log.Println(err)
+	}
+
+	server := http.Server{
+		Addr:    env.ApplicationPort,
+		Handler: rls,
+	}
+	if err := server.ListenAndServe(); err != nil {
+		log.Panicf("Server error: %v", err)
+	}
+}
